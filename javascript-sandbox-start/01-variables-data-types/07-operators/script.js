b3791be457334eeb12ca202a4019522fe4d3ce18
Al <acordero3214@hotmail.com>
2024-01-23 17:50:21 -0600

    Finish lesson on Arithmetic and Comparison Operators

--- javascript-sandbox-start/01-variables-data-types/07-operators/script.js
+++ javascript-sandbox-start/01-variables-data-types/07-operators/script.js
@@ -1,7 +1,52 @@
 // 1. Arithmetic Operators
 
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 5 % 5;
+
+// Concatenation
+x = 'Hello' + ' ' + 'World';
+
+// Exponent
+x = 2 ** 3;
+
+// Increment
+x = 1;
+// x = x + 1;
+x++;
+
+// Decerement
+// x = x - 1;
+x--;
 
 // 2. Assignment Operators
 
+x = 10;
+
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 5;
+x **= 5;
 
 // 3. Comparison Operators
+
+x = 2 == 2; // only checks value
+
+x = 2 === 2; // Strict equal, checks type and value
+
+x = 2 != 2; // only checks value
+
+x = 2 !== 2; // Strict equal, checks type and value
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
+
+console.log(x);
